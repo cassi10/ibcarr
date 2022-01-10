67f64c704c5d10dae42a6f1f4bb39391ed181284
cassi10 <67207048+cassi10@users.noreply.github.com>
2022-01-10 02:46:04 +0000

    remove clean: true

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "tsup";
 
 export default defineConfig((options) => {
   return {
-    clean: true,
     format: ["cjs", "esm"],
     dts: true,
     external: ["react"],
