2bf2c1e0062e6f1da0d5841f0ce968dcb4dbb692
cassi10 <67207048+cassi10@users.noreply.github.com>
2022-01-14 15:17:23 +0000

    Updated top bar

--- packages/ui/src/top-bar.tsx
+++ packages/ui/src/top-bar.tsx
@@ -1,12 +1,32 @@
 import { Box, ColorMode, Flex, Icon, Link, Text } from "@chakra-ui/react";
 import fromColorMode from "./from-color-mode";
-import { getIcon } from "./icons";
+import { getIcon, IconsType } from "./icons";
+
+interface TopBarItemProperties {
+  icon: IconsType;
+  link: string;
+  linkText: string;
+}
+
+const TopBarItem: React.FC<TopBarItemProperties> = ({
+  icon,
+  link,
+  linkText
+}) => (
+  <>
+    <Icon as={getIcon("dot")} p={0} h={6} w={6} />
+    <Icon as={getIcon(icon)} mr={1} />
+    <Link href={link} isExternal>
+      {linkText}
+    </Link>
+  </>
+);
 
 export interface TopBarProperties {
   colorMode: ColorMode;
 }
 
-export const TopBar = ({ colorMode }: TopBarProperties): JSX.Element => {
+export const TopBar: React.FC<TopBarProperties> = ({ colorMode }) => {
   return (
     <Box
       mb={12}
@@ -15,21 +35,27 @@ export const TopBar = ({ colorMode }: TopBarProperties): JSX.Element => {
     >
       <Flex align="center" direction="row" justify="start">
         <Text>Made by Isaac Barnes-Carr</Text>
-        <Icon as={getIcon("dot")} p={0} h={6} w={6} />
-        <Icon as={getIcon("github")} mr={1} />
-        <Link href="https://github.com/cassi10/" isExternal>
-          GitHub
-        </Link>
-        <Icon as={getIcon("dot")} p={0} h={6} w={6} />
-        <Icon as={getIcon("linkedIn")} mr={1} />
-        <Link href="https://www.linkedin.com/in/isaac-barnes-carr/" isExternal>
-          LinkedIn
-        </Link>
-        <Icon as={getIcon("dot")} p={0} h={6} w={6} />
-        <Icon as={getIcon("listtask")} mr={1} />
-        <Link href="https://todolist.ibcarr.com" isExternal>
-          TodoList
-        </Link>
+
+        <TopBarItem
+          icon="github"
+          link="https://github.com/cassi10/"
+          linkText="GitHub"
+        />
+        <TopBarItem
+          icon="linkedIn"
+          link="https://www.linkedin.com/in/isaac-barnes-carr/"
+          linkText="LinkedIn"
+        />
+        <TopBarItem
+          icon="listtask"
+          link="https://todolist.ibcarr.com"
+          linkText="TodoList"
+        />
+        <TopBarItem
+          icon="games"
+          link="https://games.ibcarr.com"
+          linkText="Games"
+        />
       </Flex>
     </Box>
   );
