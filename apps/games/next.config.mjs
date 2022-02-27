a201c26fe4ed05b1d07f83039eada82274570b50
Isaac <67207048+cassi10@users.noreply.github.com>
2022-02-27 21:57:16 +0000

    did redirect from / to /hangman. and other

--- apps/games/next.config.mjs
+++ apps/games/next.config.mjs
@@ -4,7 +4,16 @@
  * @type {import("next").NextConfig}
  * */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/hangman",
+        permanent: false
+      }
+    ];
+  }
 };
 
 export default nextConfig;
--- apps/games/src/pages/index.tsx
+++ apps/games/src/pages/index.tsx
@@ -1,16 +1,6 @@
 import Head from "next/head";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 const Home = (): JSX.Element => {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push("/hangman").catch((error: unknown) => {
-      throw new Error(JSON.stringify(error));
-    });
-  }, [router]);
-
   return (
     <Head>
       <title>Games</title>
